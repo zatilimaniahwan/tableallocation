24e5bda1394cf554732cc4f685d0cf37140d3194
Nurzatilimani Muhamad Ahwan <zatilimaniahwan@gmail.com>
2020-05-28 08:32:44 +0800

    Update SidebarComponent.vue

--- laravel/resources/js/components/master/SidebarComponent.vue
+++ laravel/resources/js/components/master/SidebarComponent.vue
@@ -31,7 +31,7 @@
         >
           <a class="nav-item-hold no-pjax" :href="baseUrl+'/checkavailables'">
             <i class="nav-icon  i-File-Clipboard"></i>
-            <span class="nav-text">Table Availability</span>
+            <span class="nav-text">Table Available</span>
           </a>
           <div class="triangle"></div>
         </li>
